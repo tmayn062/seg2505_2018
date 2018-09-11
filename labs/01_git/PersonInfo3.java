287ad60b506cc3694601df6d0d6b83c08fd698d0
blafr103 <bv.lafrance@gmail.com>
2018-09-11 17:15:25 -0400

    PersonInfo3

--- labs/01_git/PersonInfo3.java
+++ labs/01_git/PersonInfo3.java
@@ -1,9 +1,9 @@
 public class PersonInfo3 {
 
-    public static String firstName = "Liz";
-    public static String lastName = "Grégoire";
-    public static int studentNo = 173819;
-    public static String description = "Likes long walks along the beach";
+    public static String firstName = "Benoit";
+    public static String lastName = "Lafrance";
+    public static int studentNo = 300015897;
+    public static String description = "yes";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
